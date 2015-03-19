3ea82c490842a7c9d831b6022ef155d3dfdcdcd0
Jarek Szczepanski <imrahil@imrahil.com>
2015-03-19 20:44:01 +0100

    Basic stuff

--- octoprint_printhistory/__init__.py
+++ octoprint_printhistory/__init__.py
@@ -8,11 +8,43 @@ __copyright__ = "Copyright (C) 2014 Jarek Szczepanski - Released under terms of 
 import octoprint.plugin
 import octoprint.events
 
-class PrintHistoryPlugin(octoprint.plugin.EventHandlerPlugin,
+class PrintHistoryPlugin(octoprint.plugin.StartupPlugin,
+                         octoprint.plugin.EventHandlerPlugin,
                          octoprint.plugin.SettingsPlugin,
                          octoprint.plugin.TemplatePlugin,
                          octoprint.plugin.AssetPlugin):
-            
-        
+
+    def on_after_startup(self):
+        self._logger.info("Hello World from Print History Plugin!")
+        self.get
+
+    def get_template_configs(self):
+        return [
+            dict(type="tab", name="History")
+        ]
+
+    #~~ EventPlugin API
+
+    def on_event(self, event, payload):
+
+        supported_event = None
+
+        if event == octoprint.events.Events.PRINT_STARTED:
+            supported_event = octoprint.events.Events.PRINT_STARTED
+
+        elif event == octoprint.events.Events.PRINT_DONE:
+            supported_event = octoprint.events.Events.PRINT_DONE
+
+        elif event == octoprint.events.Events.PRINT_CANCELLED:
+            supported_event = octoprint.events.Events.PRINT_CANCELLED
+
+        if supported_event is None:
+            return
+
+        self._logger.info("event: %s" % supported_event)
+        metadata = self._file_manager.get_metadata(payload["origin"], payload["file"])
+        self._logger.info("metadata: %s" % metadata)
+
+
 __plugin_name__ = "Print History Plugin"
 __plugin_implementations__ = [PrintHistoryPlugin()]
